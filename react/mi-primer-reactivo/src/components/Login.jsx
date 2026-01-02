740ac1d830b5eebf9e55f0c9ec3a5300df1f03bf
todopiola <benavidesgarciafranco@gmail.com>
2026-01-02 13:38:03 +0100

    Formulario de login creado

--- react/mi-primer-reactivo/src/components/Login.jsx
+++ react/mi-primer-reactivo/src/components/Login.jsx
@@ -1,18 +1,38 @@
 export default function Login (props) {
 
   const user = {
-    username: "Franco",
-    email: "franco@email.com",
+    username: "",
+    email: "",
   }
 
-  const handleClick = () => {
+  const setUsername = (e) => {
+    user.username = e.target.value
+  }
+
+  const setEmail = (e) => {
+    user.email = e.target.value
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(user)
     props.handleLogin(user)
   }
 
   return (
     <section>
       <h2>Login section</h2>
-      <button onClick={handleClick}>Login</button>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" onChange={setUsername}/>
+        </fieldset>
+        <fieldset>
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" onChange={setEmail}/>
+        </fieldset>
+        <button>Login</button>
+      </form>
     </section>
   )
 }
