20ef95dbd89ffb1605d824fd7bc88753127e8087
todopiola <benavidesgarciafranco@gmail.com>
2026-01-08 12:49:09 +0100

    Comentarios añadidos al código

--- pokemon/src/components/PokemonCard.jsx
+++ pokemon/src/components/PokemonCard.jsx
@@ -4,17 +4,17 @@ function PokemonCard (props) {
   const { pokemon } = props; // Recoge la propiedad pokemon del componente padre
 
   return (
-    pokemon.id ? (
+    pokemon.id ? (  // Comprueba si existe el id
     <li className="pokemon-card">
       <h2 className="pokemon-name">{pokemon.name}</h2>
       <img
-        src={pokemon.sprites.front_default}
+        src={pokemon.sprites.front_default} // Recoge la url de la imagen
         alt="pokemon img"
         className="pokemon-img"
       />
       <h3 className="text">HP: {pokemon.stats[0].base_stat}</h3>
     </li>
-    ) : (
+    ) : ( // Si no existe el id
       <p>Cargando...</p>
     )
   )
--- pokemon/src/components/PokemonList.jsx
+++ pokemon/src/components/PokemonList.jsx
@@ -3,14 +3,14 @@ import './PokemonList.css'
 import { useEffect, useState } from 'react'
 
 export default function PokemonList () {
-  const [pokemons, setPokemons] = useState([]);
+  const [pokemons, setPokemons] = useState([]); // Variable reactiva
 
 
   useEffect(() => {
     getPokemons(10)
   }, []) // Ejecute nada más arrancar el componente
 
-  const fetchPokemon = async (index) => {
+  const fetchPokemon = async (index) => {   // Retorna una promesa
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${index}`)
     const data = await response.json()
     return data
@@ -20,17 +20,17 @@ export default function PokemonList () {
     const pokemonArray = [];
 
     for (let i = 1; i <= quantity; i++) {
-      pokemonArray.push(await fetchPokemon(i))
+      pokemonArray.push(await fetchPokemon(i))  // Agrega el pokemon a la lista
     }
 
-    setPokemons(pokemonArray)
+    setPokemons(pokemonArray) // Actualiza la variable reactiva
   }
 
-  const pokemonCards = pokemons.map((pokemon) => {
+  const pokemonCards = pokemons.map((pokemon) => {  // Mapeo los pokemons para renderizarlos
     return <PokemonCard key={pokemon.id} pokemon={pokemon}/>
   })
 
-  return (
+  return ( // Renderiza el componente con los pokemons
     <ul className="pokemon-list">
       {pokemonCards}
     </ul>
