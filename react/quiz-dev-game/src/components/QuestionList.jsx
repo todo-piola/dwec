fd54e1a2ef7624971e7c6e4d5d7eafb8355f048b
todopiola <benavidesgarciafranco@gmail.com>
2026-01-10 14:21:47 +0100

    Añado el método para obtener (fetch) preguntas

--- react/quiz-dev-game/src/components/QuestionList.jsx
+++ react/quiz-dev-game/src/components/QuestionList.jsx
@@ -1,4 +1,30 @@
 function QuestionList () {
+  const token = "E8gicbMZ65qiMyVBRjknQDZbrDuaT2ZJZLRpimsJ"
+
+  const [questions, setQuestions] = useState([])
+
+  const fetchQuestions = async (url) => {
+    const response = await fetch(url, {
+        headers: {
+          "X-Api-Key": token
+        }
+    })
+
+    if(!response.ok) {
+      throw new Error("Error al obtener las preguntas")
+    }
+
+    const data = await response.json()
+    return data
+  }
+
+  useEffect(() => {
+    fetchQuestions("https://quizapi.io/api/v1/questions?limit=5")
+      .then(setQuestions)
+      .catch(console.error);
+  }, []); // Estos [] son para que se monte sólo una vez
+
+
   return (
     <div>QuestionList</div>
   )
