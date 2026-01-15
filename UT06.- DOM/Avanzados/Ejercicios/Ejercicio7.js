8daacd5434b9801e81c36881564bff89b9a76274
todopiola <benavidesgarciafranco@gmail.com>
2026-01-16 00:26:47 +0100

    Mejoras de legibilidad en el código

--- UT06.- DOM/Avanzados/Ejercicios/Ejercicio7.js
+++ UT06.- DOM/Avanzados/Ejercicios/Ejercicio7.js
@@ -1,39 +1,57 @@
-let selectedPosition = null;
+import {updatePanel} from './Ejercicio8.js'
+
+let coordenadaSeleccionada = null;
 let target = ""
-let color = null
+export let color = null
+export let contadorMovimientos = 0
+
+const PIECES = {
+  WHITE: "♙",
+  BLACK: "♟"
+};
+
+function limpiarHighlights() {
+  document.querySelectorAll(".highlight")
+    .forEach(sq => sq.classList.remove("highlight"));
+}
+
+function movePiece(from, to, piece) {
+  from.textContent = "";
+  if(color) to.textContent = PIECES.WHITE
+  else to.textContent = PIECES.BLACK
+  contadorMovimientos++;
+}
+
+
 
 board.addEventListener("click", (event) => {
   const square = event.target;
+
   if (!square.classList.contains("square")) return;
 
   if(square.classList.contains("highlight")) {
-    if(color) {
-      square.textContent = "♙"
-      target.classList.remove("highlight");
-      return
-    }
-    else if(!color) {
-      square.textContent = "♟"
-      target.classList.remove("highlight");
-      return
-    }
+    const casillaOrigen = document.querySelector(`[data-pos="${coordenadaSeleccionada}"]`);
+
+    movePiece(casillaOrigen, square, square.textContent);
+    limpiarHighlights();
+    return  // importante salir del evento para que no se active otra vez el highlight una vez se ha movido la pieza
   }
 
-  document.querySelectorAll(".highlight")
-    .forEach(sq => sq.classList.remove("highlight"));
+  // Si es peón...
+  if (square.textContent === PIECES.WHITE || square.textContent === PIECES.BLACK) {
+    const piece = square.textContent;
+    color = piece === PIECES.WHITE; //color es true si el contenido de la casilla pulsada es peón blanco, si no color es false
 
-  if (square.textContent === "♙" || square.textContent === "♟") {
     let nextRank = ""
-    selectedPosition = square.dataset.pos;
-    if(square.textContent === "♙") color = true
-    else color = false
+    coordenadaSeleccionada = square.dataset.pos;
 
-    const col = selectedPosition[0];
-    const fila = selectedPosition[1];
+    const col = coordenadaSeleccionada[0];
+    const fila = coordenadaSeleccionada[1];
+
     if(color) nextRank = parseInt(fila) + 1;
     else nextRank = parseInt(fila) - 1;
 
-    target = document.querySelector(`[data-pos="${col}${nextRank}"]`);
-    if (target.textContent === "") target.classList.add("highlight");
+    target = document.querySelector(`[data-pos="${col}${nextRank}"]`); // se almacena la casilla posterior al peón
+    if (target.textContent === "") target.classList.add("highlight");   // si esa casilla está vacía, se subraya destacado
   }
 });
