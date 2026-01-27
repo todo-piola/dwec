6ad98169915cfad31f60305d599475c2d3596c42
todopiola <benavidesgarciafranco@gmail.com>
2026-01-27 21:17:52 +0100

    Renombrando directorios y ficheros

--- UT06.- DOM/Avanzados/DOM3/Ejercicio1.js
+++ UT06.- DOM/Avanzados/DOM3/Ejercicio1.js
@@ -1,4 +1,18 @@
-let state = {
-  value: ""
+export const gameStatePeones = {
+  peones : Array(8).fill("♙")
 }
 
+export const peonesColumna = document.getElementById("peones")
+
+export function renderPawns(gameState) {
+  peonesColumna.textContent = ""
+
+  for(const pawn of gameState.peones) {
+    const p = document.createElement("p")
+    p.textContent = pawn
+    p.style.margin = "5px"
+    peonesColumna.append(p)
+  }
+}
+
+renderPawns(gameStatePeones)
