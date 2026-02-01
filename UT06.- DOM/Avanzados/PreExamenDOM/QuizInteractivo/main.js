960581f7b2af3c87fa1d82b1d1292fe9116f3211
todopiola <benavidesgarciafranco@gmail.com>
2026-02-01 22:58:59 +0100

    Quiz interactivo

---
+++ UT06.- DOM/Avanzados/PreExamenDOM/QuizInteractivo/main.js
@@ -0,0 +1,41 @@
+
+const preguntasTech = {
+  "¿Cuál es el resultado de '5' + 3 en JavaScript?":
+    ["'53'", "8", "TypeError", "NaN"],
+
+  "¿Qué técnica permite que un elemento hijo transmita un evento a su padre para ser manejado allí?":
+    ["Event Delegation", "Event Bubbling", "Hoisting", "Strict Mode"],
+
+  "En el DOM, ¿cuál es la diferencia principal entre 'textContent' e 'innerHTML'?":
+    ["'textContent' devuelve solo texto, 'innerHTML' devuelve el marcado HTML", "'innerHTML' es más rápido", "No hay diferencia", "'textContent' permite ejecutar scripts"],
+
+  "¿Qué estructura de datos sigue el principio LIFO (Last In, First Out)?":
+    ["Stack (Pila)", "Queue (Cola)", "Linked List", "Array"],
+
+  "Si una función se llama a sí misma hasta alcanzar una condición de parada, estamos ante una:":
+    ["Recursión", "Iteración infinita", "Clausura (Closure)", "Callback"]
+};
+
+const pregunta = document.getElementById("#pregunta");
+const respuestas = document.querySelectorAll("#respuesta1, #respuesta2, #respuesta3, #respuesta4");
+const btnSiguiente = document.getElementById("#siguiente");
+
+let contador = 0
+let arrayPreguntas = Object.keys(preguntasTech);
+let arrayRespuestas = Object.values(preguntasTech);
+
+document.addEventListener("DOMContentLoaded", () => {
+  while(contador < arrayPreguntas.length) {
+    mostrarPregunta(pregunta, arrayRespuestas, contador);
+
+    contador++
+  }
+});
+
+function mostrarPregunta(pregunta, arrayRespuestas, contador){
+  pregunta.textContent = arrayPreguntas[contador];
+
+  for (let i = 0; i < 4; i++) {
+    respuestas[i].textContent = arrayRespuestas[contador][i];
+  }
+}
